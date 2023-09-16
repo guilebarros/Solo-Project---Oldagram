016cb81f7df4e973e147f2332a3a349696a8aa4b
guilebarros <barros.guilherme@gmail.com>
2023-09-16 10:12:56 -0300

    Increasing likes and other profiles
    Done

--- index.js
+++ index.js
@@ -17,38 +17,56 @@ const posts = [
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
-        {
+    {
         name: "Joseph Ducreux",
-        username: "jd1735",
+        username: "Russeau",
         location: "Paris, France",
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
+    },
+    {
+        name: "Frida Kahlo",
+        username: "rivera1934",
+        location: "Coyoacan, Mexico",
+        avatar: "images/avatar-kahlo.png",
+        post: "images/post-kahlo.png",
+        comment: "Love ya, guys. More details in bio.",
+        likes: 20234
+    },
+
+    {
+        name: "Andy Warhol",
+        username: "loureed1945",
+        location: "New York, USA",
+        avatar: "images/avatar-warhol.png",
+        post: "images/post-warhol.webp",
+        comment: "Your 15 minutes of fame. :)",
+        likes: 2212
     }
 ]
 
 // Function to create and append a profile to the container
 function createProfile(profile) {
-    const container = document.querySelector('.container');
-
-    // Create profile div
-    const profileDiv = document.createElement('div');
-    profileDiv.classList.add('profile');
+    // Create a unique identifier for each profile container
+    const profileContainer = document.createElement('div')
+    profileContainer.id = profile.username;
+    profileContainer.classList.add('profile')
 
     // Create profile avatar image
-    const avatarImg = document.createElement('img');
+    const avatarImg = document.createElement('img')
     avatarImg.src = profile.avatar;
-    avatarImg.classList.add('profile-avatar');
+    avatarImg.classList.add('profile-avatar')
 
     // Create profile information div
-    const infoDiv = document.createElement('div');
-    infoDiv.classList.add('profile-info');
+    const infoDiv = document.createElement('div')
+    infoDiv.classList.add('profile-info')
 
     // Create profile name paragraph
-    const namePara = document.createElement('p');
-    namePara.classList.add('name');
-    namePara.textContent = profile.name;
+    const namePara = document.createElement('p')
+    namePara.classList.add('name')
+    namePara.textContent = profile.name
 
     // Create profile location paragraph
     const locationPara = document.createElement('p');
@@ -56,62 +74,74 @@ function createProfile(profile) {
     locationPara.textContent = profile.location;
 
     // Create post image
-    const postImg = document.createElement('img');
+    const postImg = document.createElement('img')
     postImg.src = profile.post;
-    postImg.classList.add('photo-post');
+    postImg.classList.add('photo-post')
 
     // Create icon buttons div
-    const iconButtonsDiv = document.createElement('div');
-    iconButtonsDiv.classList.add('icon-buttons');
+    const iconButtonsDiv = document.createElement('div')
+    iconButtonsDiv.classList.add('icon-buttons')
 
-    const heartButton = createIconButton('images/icon-heart.png');
-    const commentButton = createIconButton('images/icon-comment.png');
-    const dmButton = createIconButton('images/icon-dm.png');
+    const heartButton = createIconButton('images/icon-heart.png')
+    heartButton.addEventListener('click', () => {
+        handleLikeClick(profile.username)
+    });
 
-    iconButtonsDiv.appendChild(heartButton);
-    iconButtonsDiv.appendChild(commentButton);
-    iconButtonsDiv.appendChild(dmButton);
+    const commentButton = createIconButton('images/icon-comment.png')
+    const dmButton = createIconButton('images/icon-dm.png')
+
+    iconButtonsDiv.appendChild(heartButton)
+    iconButtonsDiv.appendChild(commentButton)
+    iconButtonsDiv.appendChild(dmButton)
 
     // Create likes paragraph
-    const likesPara = document.createElement('p');
-    likesPara.classList.add('likes');
-    likesPara.textContent = `${profile.likes} likes`;
+    const likesPara = document.createElement('p')
+    likesPara.classList.add('likes')
+    likesPara.textContent = `${profile.likes} likes`
 
     // Create comments paragraph
-    const commentsPara = document.createElement('p');
-    commentsPara.classList.add('comments');
-    commentsPara.innerHTML = `<span class="user-comment">${profile.username}</span>${profile.comment}`;
+    const commentsPara = document.createElement('p')
+    commentsPara.classList.add('comments')
+    commentsPara.innerHTML = `<span class="user-comment">${profile.username}</span>${profile.comment}`
 
     // Append elements to profile info div
-    infoDiv.appendChild(namePara);
-    infoDiv.appendChild(locationPara);
+    infoDiv.appendChild(namePara)
+    infoDiv.appendChild(locationPara)
+    profileContainer.appendChild(avatarImg)
+    profileContainer.appendChild(infoDiv)
+    profileContainer.appendChild(postImg)
+    profileContainer.appendChild(iconButtonsDiv)
+    profileContainer.appendChild(likesPara)
+    profileContainer.appendChild(commentsPara)
 
-    // Append elements to profile div
-    profileDiv.appendChild(avatarImg);
-    profileDiv.appendChild(infoDiv);
-    profileDiv.appendChild(postImg);
-    profileDiv.appendChild(iconButtonsDiv);
-    profileDiv.appendChild(likesPara);
-    profileDiv.appendChild(commentsPara);
-
-    // Append profile div to the container
-    container.appendChild(profileDiv);
+    // Append profile container to the profiles container
+    const profilesContainer = document.querySelector('#profiles-container')
+    profilesContainer.appendChild(profileContainer)
 }
-
-// ...rest of the code remains the same...
-
 
 // Function to create an icon button
 function createIconButton(iconSrc) {
-    const button = document.createElement('button');
-    const iconImg = document.createElement('img');
+    const button = document.createElement('button')
+    const iconImg = document.createElement('img')
     iconImg.src = iconSrc;
-    iconImg.classList.add('icon-btn');
-    button.appendChild(iconImg);
-    return button;
+    iconImg.classList.add('icon-btn')
+    button.appendChild(iconImg)
+    return button
+}
+
+// Function to handle the like button click event
+function handleLikeClick(username) {
+    const profile = posts.find((p) => p.username === username)
+    if (profile) {
+        profile.likes++
+        const likesPara = document.querySelector(`#${username} .likes`)
+        if (likesPara) {
+            likesPara.textContent = `${profile.likes} likes`
+        }
+    }
 }
 
 // Loop through the posts array and create profiles
 for (const profile of posts) {
-    createProfile(profile);
+    createProfile(profile)
 }
